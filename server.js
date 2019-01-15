fd19f8ca4611ac45d4ffd7b46d2d17025caccbc2
moto8xpk <35144476+moto8xpk@users.noreply.github.com>
2019-01-16 00:39:31 +0700

    commit
    commit

--- server.js
+++ server.js
@@ -31,4 +31,4 @@ res.sendFile(path.join(__dirname+'/dist/shopping-store/index.html'));
 
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080, () => console.log(`server started!`));
+app.listen(process.env.PORT || 8080);

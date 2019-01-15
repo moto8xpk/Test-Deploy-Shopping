735b0e11c4f4986fb9801d9b9345f64032a2347e
moto8xpk <35144476+moto8xpk@users.noreply.github.com>
2019-01-16 02:30:34 +0700

    update
    update

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.static(__dirname + '/dist/ShoppingStore'));
 
 const path = require('path');
 
-app.get('/*all', function(req,res) {
+app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/ShoppingStore/index.html'));
 });
